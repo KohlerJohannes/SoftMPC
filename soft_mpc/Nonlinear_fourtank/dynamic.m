eea616ee303de7057715d0dce5b3a48c3d8591d5
Johannes Köhler <112003408+KohlerJohannes@users.noreply.github.com>
2025-12-09 16:41:28 +0100

    Delete soft_mpc directory

--- soft_mpc/Nonlinear_fourtank/dynamic.m
+++
@@ -1,8 +0,0 @@
-function x_new=dynamic(x,u,param)
-%RK4 dynamics
-k1 = fun_c(x,u,param);
-k2 = fun_c(x+param.delta/2*k1,u,param);
-k3 = fun_c(x+param.delta/2*k2,u,param);
-k4 = fun_c(x+param.delta*k3,u,param);
-x_new = x+param.delta/6*(k1+2*k2+2*k3+k4);
-end
